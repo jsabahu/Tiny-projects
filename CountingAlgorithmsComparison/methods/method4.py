c01cf20afbacf3f63efd03984f71f7b2be26f49f
jsabahu <jordisabates1996@hotmail.com>
2024-02-04 18:23:37 +0100

    added description to funcitons

--- CountingAlgorithmsComparison/methods/method4.py
+++ CountingAlgorithmsComparison/methods/method4.py
@@ -2,6 +2,17 @@ import time
 
 def method4(unsorted: list) -> (list, float):
     '''
+      Given an unsorted list of numbers, this function computes the frequency count of each unique element and returns a list of lists containing the unique elements along with their corresponding counts. 
+      It uses a dictionary to count the frequency. This method only goes through the list once.
+      Additionally, it measures the time taken for this computation.
+
+    Args:
+        unsorted (list): An unsorted list of numbers.
+
+    Returns:
+        tuple: A tuple containing two elements:
+            - A list of lists, where each inner list contains two elements: the unique element and its count in the input list.
+            - A float representing the time taken for the computation in seconds.
     '''
     t0 = time.time()
     d = {}
--- CountingAlgorithmsComparison/methods/methodNativeDict.py
+++ CountingAlgorithmsComparison/methods/methodNativeDict.py
@@ -2,6 +2,18 @@ import time
 
 def methodNativeDict(unsorted: list) -> (list, float):
     '''
+    Given an unsorted list of numbers, this function computes the frequency count of each unique element and returns a list of lists containing the unique elements along with their corresponding counts. 
+    It uses a dictionary and the built-in function 'count'.
+    Additionally, it measures the time taken for this computation.
+    Counts the frequency of elements in an unsorted list using a dictionary and the built-in function 'count'.
+
+    Args:
+        unsorted (list): An unsorted list of elements.
+
+    Returns:
+        tuple: A list of lists containing unique elements and their corresponding frequencies,
+               and the time taken for the operation in seconds.
+
     '''
     t0 = time.time()
     
